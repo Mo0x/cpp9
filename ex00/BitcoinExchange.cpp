f563b6baa22a2b881ef9d6dc7b04ffa01a2e7ecf
Mo0x <max.govindaraju@gmail.com>
2025-02-06 18:11:32 +0100

    good

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*   BitcoinExchange.cpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mgovinda <mgovinda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 17:41:16 by mgovinda          #+#    #+#             */
-/*   Updated: 2025/02/06 18:03:08 by mgovinda         ###   ########.fr       */
+/*   Updated: 2025/02/06 18:08:58 by mgovinda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,3 +14,25 @@
 #include <iostream>
 #include <ifstream>
 
+BitcoinExchange::BitcoinExchange()
+{
+	return ;
+}
+
+BitcoinExchange::BitcoinExchange(const BitcoinExchange &src) :
+	m_rate(src.m_rate)
+{
+	return ;
+}
+
+BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &src)
+{
+	if (this != &src)
+		m_rate = src.m_rate();
+	return (*this);
+}
+
+BitcoinExchange::~BitcoinExchange()
+{
+	return ;
+}
