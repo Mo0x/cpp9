341d84176e7e59731d5d63553d74577597e18691
Mo0x <max.govindaraju@gmail.com>
2025-02-06 18:03:00 +0100

    good

--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -5,35 +5,11 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mgovinda <mgovinda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/04 17:04:32 by mgovinda          #+#    #+#             */
-/*   Updated: 2025/02/06 18:02:56 by mgovinda         ###   ########.fr       */
+/*   Created: 2025/02/06 17:41:16 by mgovinda          #+#    #+#             */
+/*   Updated: 2025/02/06 17:41:37 by mgovinda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef BITCOINEXCHANGE_HPP
-#define BITCOINEXCHANGE_HPP
-
+#include "BitcoinExchange.hpp"
+#include <iostream>
 #include <ifstream>
-#include <map>
-
-class BitcoinExchange
-{
-	private:
-	std::map<std::string, double> m_rate;
-	bool is_valid_data(const std::string &date) const;
-	bool is_valid_value(const std::string &value) const;
-
-	public:
-	BitcoinExchange()
-	BitcoinExchange(const BitcoinExchange &src);
-	BitcoinExchange &operator=(const BitcoinExchange &src);
-	~BitcoinExchange();
-	
-	//member func
-	void load_data(std::string data);
-	void load_intput(std::string input);
-	double	convert(std::string &date) const;
-
-}
-
-#endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,16 +6,26 @@
 /*   By: mgovinda <mgovinda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 17:38:24 by mgovinda          #+#    #+#             */
-/*   Updated: 2025/02/04 17:45:44 by mgovinda         ###   ########.fr       */
+/*   Updated: 2025/02/06 17:40:03 by mgovinda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <ifstream>
 #include "BitcoinExchange.hpp"
 
 int main(int ac, char **av)
 {
 	if (ac != 2)
 		std::cerr << "Incorrect number of args, try ./btc input.txt" << std::endl, exit(1);
-	
+	ifstream data("./data/data.cvs")
+	if (data.fail())
+		std::cerr << "Couldnt load data file." << std::endl, exit(1);
+	ifstream input(av[1])
+	if (data.fail())
+		std::cerr << "Couldnt load input file." << std::endl, exit(1);
+	btc(input, data);
+	data.close();
+	input.close();
+	return (0);
 }
