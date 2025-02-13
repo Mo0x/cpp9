bf0f63154ceecfd7c20109a2db5c4f7181f66f1f
Mo0x <max.govindaraju@gmail.com>
2025-02-13 16:35:36 +0100

    start of ford j

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: mgovinda <mgovinda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 12:57:40 by mgovinda          #+#    #+#             */
-/*   Updated: 2025/02/13 15:14:40 by mgovinda         ###   ########.fr       */
+/*   Updated: 2025/02/13 16:35:31 by mgovinda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,11 +88,58 @@ void PmergeMe::sort_and_displayer()
 	std::clock_t end_dq = std::clock();
 	double time_dq = 10000000.0 * (end_dq - end_dq) / CLOCKS_PER_SEC;
 
-	std::cout << "Afer:";
+	std::cout << "After:";
 	for (size_t i = 0; i < vec.size(); i++)
 		std::cout <<  " " << vec[i];
 	std::cout << std::endl;
 	
 	std::cout << "Time to process a range of " << m_input.size() << "elements with std::vector: " << time_vec << " µs" << std::endl;
 	std::cout << "Time to process a range of " << m_input.size() << "elements with std::deque: " << time_vec << " µs" << std::endl;
+}
+
+void PmergeMe::merge_insertion_sort_vec(std::vector<int> &vec)
+{
+	if (vec.size() < 1)
+		return ;
+	std::vector<int> chain1, chain2;
+	size_t i;
+	for (i = 0; i + 1 < vec.size(); i += 2)
+	{
+		if (vec[i] > vec[i + 1])
+		chain1.push_back(vec[i +1]);
+		chain2.push_back(vec[i]);
+	}
+	if (i < vec.size())
+		chain2.push_back(vec[i]);
+	merge_insertion_sort_vec(chain1);
+	std::vector<size_t> order = generate_jacob(chain2.size());
+	for (size_t j = 0; j < chain1.size(); j++)
+	{
+		int element = chain2[order[j]];
+		std::vector<int>::iterator it = std::lower_bound(chain1.begin(), chain1.end(), element);
+		chain1.insert(it, element);
+	}
+	vec = chain1;
+}
+
+std::vector <size_t> PmergeMe::generate_jacob(size_t n)
+{
+	std::vector<size_t> order;
+	std::vector<size_t> jacob(2, 0);
+	jacob[1] = 1;
+	while(jacob.back() <= n)
+		jacob.push_back(jacob[jacob.size() - 1] + 2 * jacob[jacob.size() -2]);
+	for (size_t i = 2; i < jacob.size(); ++i)
+	{
+		size_t end = jacob[i];
+		size_t start = jacob[i -1] + 1;
+		if (start > n)
+			break;
+			end = std::min(end, n);
+			for (size_t j = end; j >= start; --j)
+				order.push_back(j - 1);
+	}
+	if (order.empty() || order[0] != 0)
+		order.insert(order.begin(), 0);
+	return order;
 }
